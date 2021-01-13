70c05710d9f49b5921d406ba606699f6455f5eb4
vuls7 <simonzap30@gmail.com>
2021-01-12 23:40:25 -0500

    finished

--- src/infraestructure/UserAuthentication/auth.module.ts
+++ src/infraestructure/UserAuthentication/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { QueryUser } from 'src/application/UserAuthentication/Query/query-user-handler';
 import UserLoginManagement from 'src/application/UserAuthentication/Command/auth-user-handler';
-import UserAuthenticationService from 'src/domain/UserActions/UserAuthentication/Service/UserAuthenticationService';
+import { UserAuthenticationService } from 'src/domain/UserActions/UserAuthentication/Service/UserAuthenticationService';
 import { UserModule } from '../Users/user.module';
 import UserAuthenticationController from './Controllers/auth.controller';
 import { MergeDaoAdapter } from './providers/dao/dao-auth.provider';
--- src/infraestructure/infra.module.ts
+++ src/infraestructure/infra.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './Users/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import UserAuthenticationModule from './UserAuthentication/auth.module';
 import { AppointmentModule } from './Appointments/appointment.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { dbConfiguration } from './Configuration/Database/config';
 import { PaymentsModule } from './Payments/payments.module';
 import { AppLogger } from './Configuration/Logger/AppLogger';
 import { TrmModule } from './TRM/trm.module';
+import UserAuthenticationModule from './UserAuthentication/auth.module';
 @Module({
-  imports: [AppLogger, UserModule, UserAuthenticationModule, AppointmentModule, PaymentsModule,TrmModule,
+  imports: [AppLogger, UserModule, UserAuthenticationModule, AppointmentModule, PaymentsModule, TrmModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: dbConfiguration,
